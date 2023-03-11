51835c1db677cc98511a4b822fe3d68f8eb78eef
Fadi <fadi@Fadis-MacBook-Pro.local>
2023-03-11 20:18:22 +0200

    add signup

---
+++ register.php
@@ -0,0 +1,42 @@
+<?php
+
+include('connection.php');
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$dob = $_POST['dob'];
+
+$usertype_id = 2;
+
+$email_verification = $mysqli->prepare('select email from users where email=?');
+$email_verification->bind_param('s', $email);
+$email_verification->execute();
+$email_verification->store_result();
+$exists = $email_verification->num_rows();
+
+$response = [];
+
+if (empty($email)) {
+    $response['response'] = "email is required";
+} else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $response['response'] = 'Inavlid email format';
+} else {
+    if (empty($password)) {
+        $response['response'] = 'Password is required';
+    }
+
+
+    if ($exists > 0) {
+        $response['response'] = "Already registred";
+    } else {
+        $query = $mysqli->prepare('insert into users(name,email,password,dob,usertype_id) values(?,?,?,?,?)');
+        $query->bind_param('ssssi', $name, $email, $password, $dob, $usertype_id);
+        $query->execute();
+        $response['response'] = 'Success';
+    }
+}
+
+echo json_encode($response)
+
+    ?>
