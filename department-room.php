03d49971c8187c25efa87688ca4828464901af4b
Fadi <fadi@Fadis-MacBook-Pro.local>
2023-03-12 20:15:22 +0200

    push data to response in multiple arrays

--- department-room.php
+++ department-room.php
@@ -19,25 +19,43 @@ $response= [
 
 if($num_rows_department > 0) {
     $query_rooms_beds =$mysqli -> prepare('SELECT rooms.id, room_number, beds.id, bed_number  from rooms inner join beds on rooms.id = beds.room_id where department_id=? and occupied = 0');
-    $query_rooms->bind_param('i', $department_id);
-    $query_rooms->execute();
-    $query_rooms->store_result();
-    $num_rows_rooms = $query_rooms -> num_rows();
-    $query_rooms -> bind_result($room_id, $room_number, $bed_id, $bed_number);
+    $query_rooms_beds->bind_param('i', $department_id);
+    $query_rooms_beds->execute();
+    $query_rooms_beds->store_result();
+    $num_rows_rooms_beds = $query_rooms_beds -> num_rows();
+    $query_rooms_beds -> bind_result($room_id, $room_number, $bed_id, $bed_number);
 
-    // if($num_rows_rooms > 0){
-    //     $query_beds =$mysqli -> prepare('SELECT id, bed_number from beds where room_id=? and occupied = 0');
-    //     $query_beds -> bind_param('i',$room_id);
-    //     $query_beds->execute();
-    //     $query_beds->store_result();
-    //     $num_rows_beds = $query_beds -> num_rows();
-    //     $query_beds -> bind_result($bed_id, $bed_number);
-    //     if($num_rows_beds == 0){
-    //         $response['']
-    //     }
-
-    //}
+    if($num_rows_rooms_beds == 0){
+        $response['response'] = "No available rooms";
+    }
+    else{
+        while($query_department -> fetch()){
+            $department_data =[
+                'department_id' => $department_id,
+                'department_name' => $department_name,
+            ];
+            array_push($response['departments'], $department_data);
+        }
+        while($query_rooms_beds -> fetch()){
+            $rooms_data =[
+                'room_id' => $room_id,
+                'room_number' => $room_number,
+            ];
+            $beds_data =[
+                'bed_id' => $bed_id,
+                'bed_number' => $bed_number,
+            ];
+            array_push($response['rooms'], $rooms_data);
+            array_push($response['beds'], $beds_data);
+        }
+        
+    }
 
 }
+else{
+    $response['response'] = 'Patient is not assigned to a hospital';
+}
+
+echo json_encode($response);
 
 ?>
