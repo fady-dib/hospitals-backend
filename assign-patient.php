9a97290a587bb1239d635ba46d69531770ae2e7f
Fadi <fadi@Fadis-MacBook-Pro.local>
2023-03-12 03:41:51 +0200

    assign patient

--- assign-patient.php
+++ assign-patient.php
@@ -2,7 +2,31 @@
 
 include('connection.php');
 
-$name = $_POST['name'];
+$patient_id = $_POST['patient_id'];
+$hospital_id = $_POST['hospital_id'];
 
+$is_active = 1;
+$date_joined = date('d-m-Y');
+
+$response= [];
+
+
+if(isset($patient_id, $hospital_id)){
+
+$query = $mysqli ->prepare('INSERT INTO hospital_users (hospital_id, user_id, is_active, date_joined) values(?,?,?,?)');
+$query->bind_param('iiss',$hospital_id, $patient_id, $is_active, $date_joined);
+if($query -> execute()){
+    $response['response'] = "Patient was succesfuly assigned";
+}
+else{
+    $response['response'] = mysqli_stmt_error($query);
+}
+}
+else{
+    $response['response']= "missing fields";
+}
+
+
+echo json_encode($response);
 
 ?>
