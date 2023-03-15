2970a9471990b172f1247884a2044c61df21da0d
Fadi <fadi@Fadis-MacBook-Pro.local>
2023-03-15 04:12:53 +0200

    add changes to patient api

--- medication-list.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include('connection.php');
-
-$user_id = $_POST['user_id'];
-
-$q
-
-?>
--- patient.php
+++ patient.php
@@ -11,40 +11,42 @@ $query_department->store_result();
 $num_rows_department = $query_department->num_rows();
 $query_department->bind_result($department_id, $department_name);
 
-$query_medications = $mysqli->prepare('SELECT id, name, cost from medications where quantity>0');
+$query_medications = $mysqli->prepare('SELECT id, name, cost,quantity from medications where quantity>0');
 $query_medications->execute();
 $query_medications->store_result();
 $num_rows_medications = $query_medications->num_rows();
-$query_medications->bind_result($medication_id, $medication_name, $medication_cost);
+$query_medications->bind_result($medication_id, $medication_name, $medication_cost, $medication_quantity);
 
 $response = [
     'departments' => [],
     'medications' => [],
 ];
-if($num_rows_department > 0){
-while ($query_department->fetch()) {
-    $department_data = [
-        'department_id' => $department_id,
-        'department_name' => $department_name,
-    ];
-    array_push($response['departments'], $department_data);
+
+ if ($num_rows_department >0){
+    while ($query_department->fetch()) {
+        $department_data = [
+            'department_id' => $department_id,
+            'department_name' => $department_name,
+        ];
+        array_push($response['departments'], $department_data);
+    }
 }
-}
-else{
-    $response['status'] = 100 ; // no available departments
+else  {
+    $response['status'] = 100; // no available departments
 }
 
-if($num_rows_medications > 0){
-    while($query_medications -> fetch()){
+
+if ($num_rows_medications > 0) {
+    while ($query_medications->fetch()) {
         $medications_data = [
             'medication_id' => $medication_id,
             'medication_name' => $medication_name,
-            'medication_cost' => $medication_cost
+            'medication_cost' => $medication_cost,
+            'medication_quantity' => $medication_quantity
         ];
         array_push($response['medications'], $medications_data);
     }
-}
-else{
+} else {
     $response['status'] = 101; // no available medictions
 }
 
